7fd09103ba4ac621a8319ceb5bc2080cd40a80c7
carlos128 <gcralos@ymail.com>
2019-05-10 12:53:00 -0500

    Update Constans.java

--- src/main/java/com/carlos/utilitis/Constans.java
+++ src/main/java/com/carlos/utilitis/Constans.java
@@ -2,21 +2,21 @@ package com.carlos.utilitis;
 
 		public class Constans {
 		// Spring Security
-		public static final String 	LOGIN_URL = "/login";
-		public static final String 	HEADER_AUTHORIZACION_KEY = "Authorization";
-		public static final String 	TOKEN_BEARER_PREFIX = "Bearer ";
+		public static final String LOGIN_URL = "/login";
+		public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
+		public static final String TOKEN_BEARER_PREFIX = "Bearer ";
 		// JWT
-		public static final String 	ISSUER_INFO = "https://www.autentia.com/";
-		public static final String 	SUPER_SECRET_KEY = "1234";
-		public static final long 	TOKEN_EXPIRATION_TIME = 864_000_000; // 10 day
+		public static final String ISSUER_INFO = "https://www.autentia.com/";
+		public static final String SUPER_SECRET_KEY = "1234";
+		public static final long   TOKEN_EXPIRATION_TIME = 864_000_000; // 10 day
 		
-		public static final String 	SECRET = "SecretKeyToGenJWTs";
-		public static final long 	EXPIRATION_TIME = 864_000_000; // 10 days
-		public static final String 	TOKEN_PREFIX = "Bearer ";
-		public static final String 	HEADER_STRING = "Authorization";
+		public static final String SECRET = "SecretKeyToGenJWTs";
+		public static final long   EXPIRATION_TIME = 864_000_000; // 10 days
+		public static final String TOKEN_PREFIX = "Bearer ";
+		public static final String HEADER_STRING = "Authorization";
 		
 		
-		public static final String 	SIGN_UP_URL = "/v1/api/signup";
+		public static final String  SIGN_UP_URL = "/v1/api/signup";
 		public static final String  USUARIO_ID="/v1/api/usuario/{id}";
 		
 		public static final String  DOCTOR ="/v1/api/doctor" ;
